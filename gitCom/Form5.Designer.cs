791633d5c54d0d412196ea9f78585fd5ed690726
Zailen03 <billyjoegregorio03@gmail.com>
2026-03-06 14:50:23 +0800

    Stex

--- gitCom/Form5.Designer.cs
+++ gitCom/Form5.Designer.cs
@@ -34,12 +34,14 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(529, 156);
+            this.button1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(145, 130);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // button2
             // 
